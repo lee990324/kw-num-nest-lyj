ae8b2061b4e48f9bb562ecc36f335cd3f73c669c
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-11-05 19:05:05 +0900

    ADD: exceptional handling

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ let command = process.argv[2];
 let numbers = process.argv
     .slice(3, process.argv.length)
     .map((n) => parseFloat(n));
+
+let result;
+switch (command) {
+    case "avg":
+        break;
+    case "prime":
+        break;
+    case "factorial":
+        break;
+    default:
+        console.log("Wrong command!");
+        process.exit(1);
+}
+
+console.log(result);
